ad29a73a5e273137321dae6ced0522547393dc4a
sojou <76433544+doanletankiet@users.noreply.github.com>
2023-10-26 07:33:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middlewares = jsonserver.defaults();
 const port = 4000;
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 server.listen(port,()=>{
     console.log('Server is running on' + port);
